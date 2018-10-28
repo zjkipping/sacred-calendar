bf47e85507e295231d26d95bc320b0f87c5c1b3a
Zachary Kipping <zjkipping@hotmail.com>
2018-10-28 15:19:21 -0500

    Updated environment config

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: '' // fill this with the production URL link locally (DON'T COMMIT)
+  API_URL: 'https://sacred-calendar.herokuapp.com/api'
 };
